72fbf22dfba097904887f8a3790fd130289df193
SagyngaliMamay <sagingali.mamayev@gmail.com>
2021-12-05 16:59:26 +0100

    testing templates & views

--- django_app/my_django_app/women/views.py
+++ django_app/my_django_app/women/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
+# So here I create something like canvas by creating a function for display it on my web page
+# then I add data(model) from models file which content come from db
+# then I put templates(structure)
+
 
 def index(request):
     return HttpResponse("Page of women app")
 
-def categories(request):
-    return HttpResponse("<h1>Articles by Categories</h1>")
+
+def categories(request, catid):
+    return HttpResponse(f"<h1>Articles by Categories</h1><p>{catid}</p>")
