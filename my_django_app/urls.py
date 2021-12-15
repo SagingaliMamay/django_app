42dacf7768050b757d7b267ebd2563fab039bb8d
SagyngaliMamay <sagingali.mamayev@gmail.com>
2021-12-15 03:26:12 +0100

    first Women migrations

--- my_django_app/urls.py
+++ my_django_app/urls.py
@@ -1,6 +1,9 @@
 # Here  I create my templates by writing the urls adresses
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from my_django_app import settings
 from women.views import categories, pageNotFound
 
 urlpatterns = [
@@ -8,4 +11,8 @@ urlpatterns = [
     path('', include('women.urls')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 handler404 = pageNotFound
